adba9c75c1753bba95ece6415a7e4e75ed52cc4e
Mochhamad Raffi Ramdhani <raffi.programming@gmail.com>
2021-12-19 10:19:23 +0700

    test 2

--- deploy.sh
+++ deploy.sh
@@ -15,9 +15,10 @@ touch .nojekyll
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+git init 
 git add -A
 git commit -m 'deploy'
+git branch -M main
 
 # if you are deploying to https://<USERNAME>.github.io
 git push -f git@github.com:mraffiramdhani/mraffiramdhani.github.io.git main
